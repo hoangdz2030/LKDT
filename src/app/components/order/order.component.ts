868134a333b15f06b17de23317e2ac6019c3e630
Hoang <huuhoang1414@gmail.com>
2024-12-03 01:53:21 +0700

    update

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/product';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiResponse } from '../../responses/api.response';
+import { OrderDTO } from '../../dtos/order/order.dto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CouponService } from '../../services/coupon.service';
 import { CartService } from '../../services/cart.service';
 import { ProductService } from '../../services/product.service';
 import { OrderService } from '../../services/order.service';
 import { TokenService } from '../../services/token.service';
 import { environment } from '../../../environments/environment';
-import { OrderDTO } from '../../dtos/order/order.dto';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/product';
 import { Order } from '../../models/order';
 import { ChangeDetectorRef } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { inject } from '@angular/core';
-import { CouponService } from '../../services/coupon.service';
-import { ApiResponse } from '../../responses/api.response';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-order',
@@ -30,149 +29,247 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-  ]
+  ],
 })
-
-export class OrderComponent implements OnInit{
-  private couponService = inject(CouponService);
-  private cartService = inject(CartService);
-  private productService = inject(ProductService);
-  private orderService = inject(OrderService);
-  private tokenService = inject(TokenService);
-  private formBuilder = inject(FormBuilder);
-  private router = inject(Router);
-
-  orderForm: FormGroup; // Đối tượng FormGroup để quản lý dữ liệu của form
-  cartItems: { product: Product, quantity: number }[] = [];
-  totalAmount: number = 0; // Tổng tiền
-  couponDiscount: number = 0; //số tiền được discount từ coupon
+export class OrderComponent implements OnInit {
+  orderForm: FormGroup;
+  cartItems: { product: Product; quantity: number }[] = [];
+  totalAmount: number = 0;
+  couponDiscount: number = 0;
   couponApplied: boolean = false;
   cart: Map<number, number> = new Map();
   orderData: OrderDTO = {
     order_id: 0,
-    user_id: 0, // Thay bằng user_id thích hợp
-    fullname: '', // Khởi tạo rỗng, sẽ được điền từ form
-    email: '', // Khởi tạo rỗng, sẽ được điền từ form
-    phone_number: '', // Khởi tạo rỗng, sẽ được điền từ form
-    address: '', // Khởi tạo rỗng, sẽ được điền từ form
+    user_id: 0,
+    fullname: '',
+    email: '',
+    phone_number: '',
+    address: '',
     status: 'pending',
-    note: '', // Có thể thêm trường ghi chú nếu cần
-    total_money: 0, // Sẽ được tính toán dựa trên giỏ hàng và mã giảm giá
-    payment_method: 'cod', // Mặc định là thanh toán khi nhận hàng (COD)
-    shipping_method: 'express', // Mặc định là vận chuyển nhanh (Express)
-    coupon_code: '', // Sẽ được điền từ form khi áp dụng mã giảm giá
-    cart_items: []
+    note: '',
+    total_money: 0,
+    payment_method: 'cod',
+    shipping_method: 'express',
+    coupon_code: '',
+    cart_items: [],
   };
 
-  constructor() {
-    // Tạo FormGroup và các FormControl tương ứng
+  constructor(
+    private couponService: CouponService,
+    private cartService: CartService,
+    private productService: ProductService,
+    private orderService: OrderService,
+    private tokenService: TokenService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.orderForm = this.formBuilder.group({
-      fullname: ['', Validators.required], // fullname là FormControl bắt buộc
-      email: ['', [Validators.email]], // Sử dụng Validators.email cho kiểm tra định dạng email
-      phone_number: ['', [Validators.required, Validators.minLength(6)]], // phone_number bắt buộc và ít nhất 6 ký tự
-      address: ['', [Validators.required, Validators.minLength(5)]], // address bắt buộc và ít nhất 5 ký tự
+      fullname: ['', Validators.required],
+      email: ['', [Validators.email]],
+      phone_number: ['', [Validators.required, Validators.minLength(6)]],
+      address: ['', [Validators.required, Validators.minLength(5)]],
       note: [''],
       couponCode: [''],
       shipping_method: ['express'],
-      payment_method: ['cod']
+      payment_method: ['cod'],
     });
   }
 
   ngOnInit(): void {
-    debugger
-    //this.cartService.clearCart();
     this.orderData.user_id = this.tokenService.getUserId();
-    // Lấy danh sách sản phẩm từ giỏ hàng
-    debugger
     this.cart = this.cartService.getCart();
-    const productIds = Array.from(this.cart.keys()); // Chuyển danh sách ID từ Map giỏ hàng
+    const productIds = Array.from(this.cart.keys());
 
-    // Gọi service để lấy thông tin sản phẩm dựa trên danh sách ID
-    debugger
-    if(productIds.length === 0) {
+    if (productIds.length === 0) {
       return;
     }
+
     this.productService.getProductsByIds(productIds).subscribe({
       next: (apiResponse: ApiResponse) => {
-        debugger
-        const products: Product[] = apiResponse.data
-        // Lấy thông tin sản phẩm và số lượng từ danh sách sản phẩm và giỏ hàng
+        const products: Product[] = apiResponse.data;
         this.cartItems = productIds.map((productId) => {
-          debugger
           const product = products.find((p) => p.id === productId);
           if (product) {
             product.thumbnail = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
           }
           return {
             product: product!,
-            quantity: this.cart.get(productId)!
+            quantity: this.cart.get(productId)!,
           };
         });
-        // console.log(this.cartItems);
       },
       complete: () => {
-        debugger;
-        this.calculateTotal()
+        this.calculateTotal();
       },
       error: (error: HttpErrorResponse) => {
-        debugger;
         console.error(error?.error?.message ?? '');
+      },
+    });
+
+    // Check if the URL has query parameters from VNPay
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (params['vnp_Amount']) {
+        this.handleVNPayReturn(params);
       }
     });
   }
+
   placeOrder() {
-    debugger
-    if (this.orderForm.errors == null) {
-      // Gán giá trị từ form vào đối tượng orderData
-      /*
-      this.orderData.fullname = this.orderForm.get('fullname')!.value;
-      this.orderData.email = this.orderForm.get('email')!.value;
-      this.orderData.phone_number = this.orderForm.get('phone_number')!.value;
-      this.orderData.address = this.orderForm.get('address')!.value;
-      this.orderData.note = this.orderForm.get('note')!.value;
-      this.orderData.shipping_method = this.orderForm.get('shipping_method')!.value;
-      this.orderData.payment_method = this.orderForm.get('payment_method')!.value;
-      */
-      // Sử dụng toán tử spread (...) để sao chép giá trị từ form vào orderData
+    if (this.orderForm.valid) {
       this.orderData = {
         ...this.orderData,
-        ...this.orderForm.value
+        ...this.orderForm.value,
       };
-      this.orderData.cart_items = this.cartItems.map(cartItem => ({
+      this.orderData.cart_items = this.cartItems.map((cartItem) => ({
         product_id: cartItem.product.id,
-        quantity: cartItem.quantity
+        quantity: cartItem.quantity,
       }));
-      this.orderData.total_money =  this.totalAmount;
-      // console.log(this.orderData)
-      // Dữ liệu hợp lệ, bạn có thể gửi đơn hàng đi
-      this.orderService.placeOrder(this.orderData).subscribe({
-        next: (response: ApiResponse) => {
-          debugger;
-          alert('Order placed successfully.');
-          this.cartService.clearCart();
-          this.saveProductsToLocalStorage();
-          this.router.navigate(['/']);
-        },
-        complete: () => {
-          debugger;
-          this.calculateTotal();
-        },
-        error: (error: HttpErrorResponse) => {
-          debugger;
-          console.error(`Lỗi khi đặt hàng: ${error?.error?.message ?? ''}`);
-        },
-      });
+      this.orderData.total_money = this.totalAmount;
+
+      // Nếu phương thức thanh toán là vnpay
+      if (this.orderData.payment_method === 'vnpay') {
+        // Lấy thông tin khách hàng từ localStorage nếu thanh toán qua VNPay
+        this.loadCustomerInfoFromStorage();
+
+        // Kiểm tra nếu có phản hồi từ VNPay (tránh gọi createVNPayPayment khi đã nhận phản hồi từ VNPay)
+        const isVNPayReturn =
+          this.router.url.includes('orders') &&
+          window.location.search.includes('vnp_Amount');
+        if (!isVNPayReturn) {
+          this.createVNPayPayment();
+        }
+      } else {
+        // Nếu phương thức thanh toán không phải vnpay, gọi API để tạo đơn hàng bình thường
+        this.orderService.placeOrder(this.orderData).subscribe({
+          next: (response: ApiResponse) => {
+            alert('Order placed successfully.');
+            this.cartService.clearCart();
+            this.saveProductsToLocalStorage();
+            this.router.navigate(['/']);
+          },
+          error: (error: HttpErrorResponse) => {
+            console.error(
+              `Error placing order: ${error?.error?.message ?? ''}`
+            );
+          },
+        });
+      }
     } else {
-      // Hiển thị thông báo lỗi hoặc xử lý khác
-      console.error('Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.');
+      console.error('Invalid form data. Please check and try again.');
     }
   }
+
+  // Handle VNPay payment creation
+  createVNPayPayment() {
+    // Lưu thông tin khách hàng trước khi chuyển đến VNPay
+    this.saveCustomerInfoToStorage();
+
+    const randomOrderNumber = Math.floor(10000000 + Math.random() * 90000000);
+    const orderInfo = `ORD${randomOrderNumber}`;
+    const returnUrl = `http://localhost:4200`;
+
+    this.orderService
+      .createPayment(this.totalAmount, orderInfo, returnUrl)
+      .subscribe({
+        next: (response: ApiResponse) => {
+          if (response.data) {
+            window.location.href = response.data;
+          } else {
+            console.error('Payment URL not returned by VNPay.');
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error(
+            'Error creating VNPay payment:',
+            error?.error?.message ?? ''
+          );
+        },
+      });
+  }
+
+  handleVNPayReturn(params: any) {
+    const request = { ...params };
+    this.orderService.paymentReturn(request).subscribe({
+      next: (response: ApiResponse) => {
+        if (response.status === 'OK') {
+          // Lấy thông tin khách hàng từ localStorage khi thanh toán thành công
+          this.loadCustomerInfoFromStorage();
+
+          // Đặt phương thức thanh toán là vnpay và tiến hành đặt hàng
+          this.orderData.payment_method = 'vnpay';
+          this.orderService.placeOrder(this.orderData).subscribe({
+            next: (response: ApiResponse) => {
+              alert('Order placed successfully.');
+              this.cartService.clearCart();
+              this.saveProductsToLocalStorage();
+              this.router.navigate(['/']);
+            },
+            error: (error: HttpErrorResponse) => {
+              console.error(
+                `Error placing order: ${error?.error?.message ?? ''}`
+              );
+            },
+          });
+        } else {
+          alert('Payment failed or invalid.');
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(
+          `Error processing VNPay return: ${error?.error?.message ?? ''}`
+        );
+      },
+    });
+  }
+
+  saveCustomerInfoToStorage() {
+    // Lấy thông tin từ form
+    const customerInfo = this.orderForm.value;
+
+    // Thêm total_money và cart_items vào customerInfo
+    const orderInfoToSave = {
+      ...customerInfo,
+      total_money: this.totalAmount, // Gán tổng tiền
+      cart_items: this.cartItems.map((cartItem) => ({
+        product_id: cartItem.product.id, // ID sản phẩm
+        quantity: cartItem.quantity, // Số lượng
+      })),
+    };
+
+    // Lưu thông tin vào localStorage
+    localStorage.setItem('customerInfo', JSON.stringify(orderInfoToSave));
+  }
+
+  loadCustomerInfoFromStorage() {
+    const savedCustomerInfo = localStorage.getItem('customerInfo');
+    if (savedCustomerInfo) {
+      const customerInfo = JSON.parse(savedCustomerInfo);
+      this.orderData = {
+        ...this.orderData,
+        fullname: customerInfo.fullname,
+        email: customerInfo.email,
+        phone_number: customerInfo.phone_number,
+        address: customerInfo.address,
+        note: customerInfo.note,
+        coupon_code: customerInfo.couponCode,
+        shipping_method: customerInfo.shipping_method,
+        payment_method: customerInfo.payment_method,
+        total_money: customerInfo.total_money,
+        cart_items: customerInfo.cart_items,
+      };
+    }
+  }
+
+  clearCustomerInfoFromStorage() {
+    localStorage.removeItem('customerInfo'); // Xóa thông tin khách hàng khỏi localStorage
+  }
+
   // lưu dữ liệu vào local storage
   saveProductsToLocalStorage() {
-
-
-
-    const existingOrders = JSON.parse(localStorage.getItem('orderedProducts') || '[]');
+    const existingOrders = JSON.parse(
+      localStorage.getItem('orderedProducts') || '[]'
+    );
     const productsToSave = this.orderData;
 
     const allOrders = [...existingOrders, productsToSave];
@@ -197,10 +294,10 @@ export class OrderComponent implements OnInit{
 
   // Hàm tính tổng tiền
   calculateTotal(): void {
-      this.totalAmount = this.cartItems.reduce(
-          (total, item) => total + item.product.price * item.quantity,
-          0
-      );
+    this.totalAmount = this.cartItems.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    );
   }
   confirmDelete(index: number): void {
     if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này?')) {
@@ -213,34 +310,28 @@ export class OrderComponent implements OnInit{
     }
   }
 
-
   // Hàm xử lý việc áp dụng mã giảm giá
   applyCoupon(): void {
     const couponCode = this.orderForm.get('couponCode')!.value;
     console.log('Mã giảm giá:', couponCode);
     if (!couponCode) {
-        return;
+      return;
     }
     this.calculateTotal();
     this.couponService.applyCoupon(couponCode, this.totalAmount).subscribe({
-        next: (response) => {
-            console.log('Kết quả giảm giá:', response.data.result);
-            this.couponDiscount = response.data.result; // Truy cập đúng cấu trúc response
-            this.totalAmount -= this.couponDiscount; // Cập nhật tổng tiền sau giảm giá
-            this.couponApplied = true; // Đánh dấu mã giảm giá đã được áp dụng
-            this.orderData.coupon_code = couponCode; // Lưu mã giảm giá vào dữ liệu đơn hàng
-        },
-        error: (error) => {
-            console.error('Lỗi khi áp dụng mã giảm giá:', error);
-            alert('Mã giảm giá không hợp lệ hoặc đã hết hạn.');
-        }
+      next: (response) => {
+        console.log('Kết quả giảm giá:', response.data.result);
+        this.couponDiscount = response.data.result; // Truy cập đúng cấu trúc response
+        this.totalAmount -= this.couponDiscount; // Cập nhật tổng tiền sau giảm giá
+        this.couponApplied = true; // Đánh dấu mã giảm giá đã được áp dụng
+        this.orderData.coupon_code = couponCode; // Lưu mã giảm giá vào dữ liệu đơn hàng
+      },
+      error: (error) => {
+        console.error('Lỗi khi áp dụng mã giảm giá:', error);
+        alert('Mã giảm giá không hợp lệ hoặc đã hết hạn.');
+      },
     });
-}
-
-
-
-
-
+  }
 
   private updateCartFromCartItems(): void {
     this.cart.clear();
@@ -249,5 +340,4 @@ export class OrderComponent implements OnInit{
     });
     this.cartService.setCart(this.cart);
   }
-
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,9 @@
-import { ProductService } from './product.service';
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpParams,
-  HttpHeaders
-} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { OrderDTO } from '../dtos/order/order.dto';
-import { OrderResponse } from '../responses/order/order.response';
 import { ApiResponse } from '../responses/api.response';
+import { OrderDTO } from '../dtos/order/order.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -18,44 +12,87 @@ export class OrderService {
   private apiUrl = `${environment.apiBaseUrl}/orders`;
   private apiGetAllOrders = `${environment.apiBaseUrl}/orders/get-orders-by-keyword`;
 
+  // VNPAY API URLs (Updated URLs)
+  private vnpayApiUrl = `${environment.apiBaseUrl}/vnpay/payment`; // Updated VNPAY create payment URL
+  private vnpayReturnUrl = `${environment.apiBaseUrl}/vnpay/paymentReturn`; // Updated VNPAY payment return URL
+
   constructor(private http: HttpClient) {}
 
   placeOrder(orderData: OrderDTO): Observable<ApiResponse> {
-    // Gửi yêu cầu đặt hàng
     return this.http.post<ApiResponse>(this.apiUrl, orderData);
   }
+
   getOrderById(orderId: number): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/orders/${orderId}`;
+    const url = `${this.apiUrl}/${orderId}`;
     return this.http.get<ApiResponse>(url);
   }
+
   getOrdersByUserId(userId: number): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/orders/user/${userId}`;
+    const url = `${this.apiUrl}/user/${userId}`;
     return this.http.get<ApiResponse>(url);
   }
-  getAllOrders(keyword:string,
-    page: number, limit: number
+
+  getAllOrders(
+    keyword: string,
+    page: number,
+    limit: number
   ): Observable<ApiResponse> {
-      const params = new HttpParams()
+    const params = new HttpParams()
       .set('keyword', keyword)
       .set('page', page.toString())
       .set('limit', limit.toString());
-      return this.http.get<ApiResponse>(this.apiGetAllOrders, { params });
+    return this.http.get<ApiResponse>(this.apiGetAllOrders, { params });
   }
+
   updateOrder(orderId: number, orderData: OrderDTO): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/orders/${orderId}`;
+    const url = `${this.apiUrl}/${orderId}`;
     return this.http.put<ApiResponse>(url, orderData);
   }
+
   deleteOrder(orderId: number): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/orders/${orderId}`;
+    const url = `${this.apiUrl}/${orderId}`;
     return this.http.delete<ApiResponse>(url);
   }
 
-   // Hàm lấy tổng doanh thu
-   getTotalRevenue(): Observable<string> {
-    return this.http.get<string>(`${environment.apiBaseUrl}/orders/total-revenue`);
+  /**
+   * Gửi yêu cầu thanh toán đến VNPAY
+   * @param amount: Số tiền thanh toán
+   * @param orderInfo: Thông tin đơn hàng
+   * @param returnUrl: URL trả về sau khi thanh toán
+   * @returns Observable<ApiResponse>
+   */
+  createPayment(
+    amount: number,
+    orderInfo: string,
+    returnUrl: string
+  ): Observable<ApiResponse> {
+    const params = new HttpParams()
+      .set('amount', amount.toString())
+      .set('orderInfo', orderInfo)
+      .set('returnUrl', returnUrl);
+
+    return this.http.get<ApiResponse>(this.vnpayApiUrl, { params });
+  }
+
+  /**
+   * Xử lý kết quả trả về từ VNPAY
+   * @param request: Yêu cầu HTTP trả về từ VNPAY
+   * @returns Observable<ApiResponse>
+   */
+  paymentReturn(request: any): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.vnpayReturnUrl, { params: request });
+  }
+
+  // Hàm lấy tổng doanh thu
+  getTotalRevenue(): Observable<string> {
+    return this.http.get<string>(
+      `${environment.apiBaseUrl}/orders/total-revenue`
+    );
   }
 
   getMonthlyRevenue(): Observable<number[]> {
-    return this.http.get<number[]>(`${environment.apiBaseUrl}/orders/monthly-revenue`);
+    return this.http.get<number[]>(
+      `${environment.apiBaseUrl}/orders/monthly-revenue`
+    );
   }
 }
